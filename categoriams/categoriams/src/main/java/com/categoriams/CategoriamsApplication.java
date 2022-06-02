cc4af248f0761577f4bc8c8c706941481fd2fbf7
Rafaela Rocha Nogueira <rafah.r.nog@gmail.com>
2022-06-02 10:29:48 -0300

    Criação do projeto

---
+++ categoriams/categoriams/src/main/java/com/categoriams/CategoriamsApplication.java
@@ -0,0 +1,13 @@
+package com.categoriams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoriamsApplication.class, args);
+	}
+
+}
---
+++ categoriams/categoriams/src/test/java/com/categoriams/CategoriamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.categoriams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
